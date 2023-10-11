bacb7ac7ff5cfa3ef95ed250da2bd132d3325cd1
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-10-11 15:15:46 -0300

    create calculate autonomy activity

---
+++ app/src/main/java/tbdbatista/eletriccardio/presentation/CalculatAutonomiaActivity.kt
@@ -0,0 +1,16 @@
+package tbdbatista.eletriccardio.presentation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import tbdbatista.eletriccardio.R
+
+class CalculatAutonomiaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calcular_autonomia)
+    }
+
+
+}
