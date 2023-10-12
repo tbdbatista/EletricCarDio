783d930067ef2445e4098a2f36e50a7fadd0485f
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-10-12 19:17:54 -0300

    implement float button for calculate activity

--- app/src/main/java/tbdbatista/eletriccardio/presentation/ui/CarroFragment.kt
+++ app/src/main/java/tbdbatista/eletriccardio/presentation/ui/CarroFragment.kt
@@ -7,14 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import tbdbatista.eletriccardio.R
 import tbdbatista.eletriccardio.presentation.data.CarroFactory
 import tbdbatista.eletriccardio.presentation.ui.adapters.CarroAdapter
 
 class CarroFragment : Fragment() {
 
-    lateinit var goToCalcularBotao: Button
+    lateinit var goToCalcularBotao: FloatingActionButton
     lateinit var listaCarros: RecyclerView
 
     override fun onCreateView(
@@ -38,13 +40,13 @@ class CarroFragment : Fragment() {
     }
     fun setupListeners() {
         goToCalcularBotao.setOnClickListener {
-//            startActivity(Intent(this, CalculatAutonomiaActivity::class.java))
+            startActivity(Intent(context, CalculaAutonomiaActivity::class.java))
         }
     }
 
     fun setupLista() {
         val adapter = CarroAdapter(CarroFactory.list)
-//        listaCarros.layoutManager = LinearLayoutManager(this)
+        listaCarros.layoutManager = LinearLayoutManager(context)
         listaCarros.adapter = adapter
     }
 }
